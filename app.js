086c5deb37b0162e64653b2ea1e0a27259fd0dc8
Dawid van der Hoven <dawidvdh@gmail.com>
2017-01-21 14:34:41 +0200

    env test

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/webhook', function (req, res) {
+	if (req.query['hub.mode'] === 'subscribe' &&
+		req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+		console.log("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error("Failed validation. Make sure the validation tokens match.");
+		res.sendStatus(403);
+	}
 });
 
 app.listen(process.env.PORT || 5000, function () {
