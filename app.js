12625f2e9120cfca0c2d2bbecfef76285cd90fec
Dawid van der Hoven <dawidvdh@gmail.com>
2017-01-21 15:33:03 +0200

    forgot about body parser

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
